970be17703549de61bc02b6b55c56bf206b5a325
i-jashanpreet <jashanpreet.sellopal@gmail.com>
2026-05-13 18:28:12 +0530

    mobile layout added

--- src/components/layout/MobileFrame.tsx
+++ src/components/layout/MobileFrame.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './MobileFrame.module.css';
+import { BatteryMedium, Wifi, Signal } from 'lucide-react';
 
 interface MobileFrameProps {
   children: React.ReactNode;
@@ -8,6 +9,18 @@ interface MobileFrameProps {
 const MobileFrame: React.FC<MobileFrameProps> = ({ children }) => {
   return (
     <div className={styles.mobileFrame}>
+      <div className={styles.statusBar}>
+        <div className={styles.time}></div>
+        <div className={styles.notch}>
+          <div className={styles.camera}></div>
+          <div className={styles.speaker}></div>
+        </div>
+        <div className={styles.statusIcons}>
+          <Signal size={14} className={styles.icon} />
+          <Wifi size={14} className={styles.icon} />
+          <BatteryMedium size={16} className={styles.icon} />
+        </div>
+      </div>
       <div className={styles.appContainer}>
         {children}
       </div>
